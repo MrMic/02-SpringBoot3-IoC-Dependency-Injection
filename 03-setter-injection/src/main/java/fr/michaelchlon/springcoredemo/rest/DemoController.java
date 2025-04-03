487efbf6b248eb938da8a9b981e8498e3d4a4348
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-03 15:57:11 +0200

    feat: 012 - Setter Injection

---
+++ 03-setter-injection/src/main/java/fr/michaelchlon/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package fr.michaelchlon.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.michaelchlon.springcoredemo.common.Coach;
+
+@RestController
+public class DemoController {
+  // Define a private field for the dependency
+  private Coach myCoach;
+
+  @Autowired
+  public void setCoach(Coach theCoach) {
+    myCoach = theCoach;
+  }
+
+  // Define an endpoint to return the daily workout
+  @GetMapping("/dailyworkout")
+  public String getDailyWorkout() {
+    return myCoach.getDailyWorkout();
+  }
+}
